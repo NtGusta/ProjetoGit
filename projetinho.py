d0b7f3df2609e45f0887a4bdf621f5a0b9bd20a1
Gustavo <dias.gustavo2017@gmail.com>
2025-03-18 19:22:31 -0300

    projetinho.py

--- projetinho.py
+++ projetinho.py
@@ -1,1 +1,38 @@
-opa aqui vai
+import requests
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+
+
+    def test_000_alunos_retorna_lista(self):
+        #pega a url /alunos, com o verbo get
+        r = requests.get('http://localhost:5002/alunos')
+
+        #o status code foi pagina nao encontrada?
+        if r.status_code == 404:
+            self.fail("voce nao definiu a pagina /alunos no seu server")
+
+        try:
+            obj_retornado = r.json()
+            #r.json() é o jeito da biblioteca requests
+            #de pegar o arquivo que veio e transformar
+            #em lista ou dicionario.
+            #Vou dar erro se isso nao for possivel
+        except:
+            self.fail("queria um json mas voce retornou outra coisa")
+
+        #no caso, tem que ser uma lista
+        self.assertEqual(type(obj_retornado),type([]))
+
+def test_106_id_inexistente(self):
+        r_reset = requests.post('http://localhost:5002/reseta')
+        self.assertEqual(r_reset.status_code,200)
+        r = requests.put('http://localhost:5002/professores/15',json={'nome':'bowser','id':15})
+        self.assertEqual(r.status_code,400)
+        self.assertEqual(r.json()['erro'],'professor nao encontrado')
+        r = requests.get('http://localhost:5002/professores/15')
+        self.assertEqual(r.status_code,400)
+        self.assertEqual(r.json()['erro'],'professor nao encontrado')
+        r = requests.delete('http://localhost:5002/professores/15')
+        self.assertEqual(r.status_code,400)
+        self.assertEqual(r.json()['erro'],'professor nao encontrado')
